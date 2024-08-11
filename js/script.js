7e11c4511863fdc6a68565368a74644befc11fbc
Vladyslav-Miroshnichenko <bjlag.mupoh@gmail.com>
2024-08-11 14:33:10 +0200

    form

--- js/script.js
+++ js/script.js
@@ -19,3 +19,31 @@ const countdown = setInterval(() => {
     document.getElementById("countdown").innerHTML = "Поздравляем!";
   }
 }, 1000);
+
+(function () {
+  emailjs.init("service_9ee9799");
+})();
+
+document
+  .getElementById("form-label")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const templateParams = {
+      attendance: document.querySelector('input[name="attendance"]:checked')
+        .value,
+      name: document.querySelector('input[name="name"]').value,
+      phone: document.querySelector('input[name="phone"]').value,
+      message: document.querySelector('input[name="message"]').value,
+    };
+    emailjs.send("service_9ee9799", "template_affdyt8", templateParams).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("Сообщение отправленно");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        alert("Ошибка при отправки сообщения");
+      }
+    );
+  });
